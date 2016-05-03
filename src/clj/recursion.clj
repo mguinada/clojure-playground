3c845c9df6fb46baf38c4ab0bb52dc22d0fc3b19
Miguel Guinada <mguinada@gmail.com>
2016-05-03 23:59:44 +0100

    recursion playground

---
+++ src/clj/recursion.clj
@@ -0,0 +1,23 @@
+(ns clojure-playground.recursion)
+
+;; refursive factorial implementations
+(defn fact
+  [n]
+  (loop [a n b 1]
+    (if (= a 1)
+      b
+      (recur (dec a) (* a b)))))
+
+(fact 10)
+
+;; a data recursive alternative base on lazy seqs!
+;; in clojure this is usually more eficiente than recursion
+(defn fact
+  [n]
+  (reduce * (range 1 (inc n))))
+
+(fact 7)
+
+;; Fibonacci infinite lazy sequence using co-recursion
+;; See more at https://en.wikibooks.org/wiki/Clojure_Programming/Examples/Lazy_Fibonacci
+(def fibo (lazy-cat [0 1] (map + fibo (rest fibo))))
