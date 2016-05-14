3d722b490bf4f307c64a2622344662cfdbe8c0c4
Miguel Guinada <mguinada@gmail.com>
2016-05-14 19:42:09 +0100

    Refactors euler p1

--- src/clj/playground/euler/p1.clj
+++ src/clj/playground/euler/p1.clj
@@ -1,15 +1,18 @@
 (ns playground.euler.p1)
 
-(defn multiple-of-3-or-5?
-  [n]
-  (some zero? (map #(mod n %) '(3 5))))
+(comment
+  "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+  Find the sum of all the multiples of 3 or 5 below 1000.")
 
-(defn multiples-of-3-or-5-bellow-n
-  [n]
-  (filter multiple-of-3-or-5? (range n)))
+(defn multiple-of?
+  "Returns true if n is a multiple of m.
+  If varadiac m is given, this predicate will return true
+  when the first m that is multiple of n is computed or false if none."
+  ([n] true)
+  ([n m] (zero? (rem n m)))
+  ([n m & more]
+   (boolean (some true? (map (partial multiple-of? n) (conj more m))))))
 
-(defn sum-of-multiples-of-3-or-5-bellow-n
-  [n]
-  (reduce + (multiples-of-3-or-5-bellow-n n)))
+(defn multiples-of-3-or-5 [n] (filter #(multiple-of? % 3 5) (range n)))
 
-(sum-of-multiples-of-3-or-5-bellow-n 1000)
+(reduce + (multiples-of-3-or-5 1000))
