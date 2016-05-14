9c289247b61fa02432f81d1bc4d445dfd4470483
Miguel Guinada <mguinada@gmail.com>
2016-05-14 22:19:15 +0100

    Euler p3 solution

--- src/clj/playground/euler/p3.clj
+++ src/clj/playground/euler/p3.clj
@@ -1,28 +1,20 @@
 (ns playground.euler.p3)
 
-;; https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-;;
-;; 1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
-;; 2. Initially, let p equal 2, the smallest prime number.
-;; 3. Enumerate the multiples of p by counting to n from 2p in increments of p, and mark them in the list (these will be 2p, 3p, 4p, ... the p itself should not be marked).
-;; 4. Find the first number greater than p in the list that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
+(comment
+  "The prime factors of 13195 are 5, 7, 13 and 29.
+   What is the largest prime factor of the number 600851475143 ?")
 
-(defn primes-recurr
+(defn divisors
   [n]
-  (loop [p 2 coll (range 2 (inc n))]
-    (if (nil? (first (drop-while (partial >= p) coll)))
-      coll
-      (recur (inc p) (remove #(and (zero? (mod % p)) (not= % p)) coll)))))
+  (sort (into [n] (filter #(zero? (rem n %)) (range 1 (inc (long (Math/sqrt n))))))))
 
-(primes-recurr 600)
+(defn prime?
+  "Predicate that returns true if n is a prime number"
+  [n]
+  (and (> n 1) (= (set [1 n]) (set (divisors n)))))
 
-;; Prime numbers lazy seq
-(defn primes
-  ([]
-   (primes (iterate inc 2)))
-  ([seq]
-   (cons (first seq)
-         (lazy-seq (primes (filter #(not= 0 (mod % (first seq))) (rest seq)))))))
+(defn prime-factors
+  [n]
+  (filter prime? (divisors n)))
 
-(take 109 (primes))
-(nth (primes) 108)
+(last (prime-factors 600851475143))
