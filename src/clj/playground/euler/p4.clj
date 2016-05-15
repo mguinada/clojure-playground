ff4afe55721483eef443a3bb66eef506615c3e41
Miguel Guinada <mguinada@gmail.com>
2016-05-15 13:28:50 +0100

    Euler p4 code gardening

--- src/clj/playground/euler/p4.clj
+++ src/clj/playground/euler/p4.clj
@@ -2,8 +2,9 @@
   (:require [clojure.string :as string]))
 
 (comment
-  "A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-  Find the largest palindrome made from the product of two 3-digit numbers.")
+  "A palindromic number reads the same both ways. The largest palindrome made from the product of
+  two 2-digit numbers is 9009 = 91 × 99. Find the largest palindrome made from the product of two
+  3-digit numbers.")
 
 (defn palindrome?
   [n]
@@ -11,6 +12,7 @@
     (= (reverse nstr) (seq nstr))))
 
 (def three-digit-products
-  (for [n1 (range 100 1000) n2 (range 100 1000)] (* n1 n2)))
+  (let [r (range 100 1000)]
+    (for [n1 r n2 r] (* n1 n2))))
 
 (apply max (filter palindrome? three-digit-products))
