35ac25c10f60146776b1a21a6cb450265b109124
Miguel Guinada <mguinada@gmail.com>
2016-05-25 22:03:26 +0100

    Add missing problem texts

--- src/clj/playground/euler/p10.clj
+++ src/clj/playground/euler/p10.clj
@@ -1,4 +1,8 @@
 (ns playground.euler.p10
   (:require [playground.euler.p7 :as p7]))
 
+(comment
+  "The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+   Find the sum of all the primes below two million.")
+
 (apply + (take-while #(< % 2000000)  (p7/primes)))
--- src/clj/playground/euler/p9.clj
+++ src/clj/playground/euler/p9.clj
@@ -1,5 +1,14 @@
 (ns playground.euler.p9)
 
+(comment
+  "A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+   a ^ 2 + b ^ 2 = c ^ 2
+   For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+   There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+   Find the product abc.")
+
 (defn pythagorean-triplet
   "Calculate the pythagorean triplet for m n with m and n being natual numbers bigger than zero
   and m > n"
